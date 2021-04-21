f2c3885523f265f15483be18c6293545f66e544a
mazan <mazanan_mail@163.com>
2021-04-21 23:38:24 +0800

    测试

--- src/env.js
+++ src/env.js
@@ -1,7 +1,8 @@
 console.log('--1212-', process.env.NODE_ENV);
 console.log('--1212-', process.env);
 
-let reqUrl = '//newdreamer.cn';
+// let reqUrl = '//newdreamer.cn';
+let reqUrl = process.env.REACT_APP_ENV === 'production' ? "//www.newdreamer.cn" : '//test.newdreamer.cn';
 
 if (window.location.href.indexOf('newdreamer.cn') <= 0) {
     reqUrl = '';
