7f9aa72caa2faa1d4d971c87c44ffd0c1c514c4a
mazan <mazanan_mail@163.com>
2021-04-21 23:11:47 +0800

    区分运行环境

--- src/env.js
+++ src/env.js
@@ -1,9 +1,10 @@
 console.log('--1212-', process.env.NODE_ENV);
+console.log('--1212-', process.env);
 
 let reqUrl = '//newdreamer.cn';
 
 if (window.location.href.indexOf('newdreamer.cn') <= 0) {
-    reqUrl = '';   
+    reqUrl = '';
 }
 
 export default reqUrl;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
 const proxy = require('http-proxy-middleware');
-
 module.exports = function (app) {
     app.use(
         proxy([
         '/newdreamer',
     ],{
-            "target": "https://newdreamer.cn",
+            "target": process.env.REACT_APP_ENV === 'production' ? "https://www.newdreamer.cn" : 'https://test.newdreamer.cn',
             changeOrigin: true,
+            secure: false,
             onProxyReq(proxyReq, req, res) {
                 proxyReq.setHeader('cookie', 'JSESSIONID=7040A5D93783E8C08BC2F055BD75DA7F');
             }
