5ce45c1b11a0aa7de5a55a5fc1b3a4152c1636ba
mz <mazan_mail@163.com>
2021-04-29 17:26:00 +0800

    优惠券页面

--- src/pages/coupon/setting/index.js
+++ src/pages/coupon/setting/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Button, Input, Select, Table, RangePicker} from 'antd/lib/index';
-
+import './index.less'
 export default class Index extends React.Component {
-    state = {
+    constructor(props) {
+      super(props);
+      this.state = {
 
-    };
-
+      }
+    }
     componentDidMount() {
 
     }
@@ -21,13 +23,7 @@ export default class Index extends React.Component {
                 name: '胡彦斌',
                 age: 32,
                 address: '西湖区湖底公园1号',
-            },
-            {
-                key: '2',
-                name: '胡彦祖',
-                age: 42,
-                address: '西湖区湖底公园1号',
-            },
+            }
         ];
 
         const columns = [
@@ -49,40 +45,28 @@ export default class Index extends React.Component {
         ];
         return (
             <div className="couponSetting">
-                <section className="product-manager-search">
+                <div className="search-box">
+                  <section className="product-manager-search">
                     <div className="manager-search-item">
-                        <div className="search-item__title">订单号</div>
-                        <Input size="small" placeholder="请输入订单号" onChange={e => this.updateSearch('orderId', e.target.value)} />
+                      <div className="search-item__title">订单号</div>
+                      <Input size="small" placeholder="请输入订单号" onChange={e => this.updateSearch('orderId', e.target.value)} />
                     </div>
                     <div className="manager-search-item">
-                        <div className="search-item__title">收货人名称</div>
-                        <Input size="small" placeholder="请输入收货人名称" onChange={e => this.updateSearch('customerName', e.target.value)} />
+                      <div className="search-item__title">收货人名称</div>
+                      <Input size="small" placeholder="请输入收货人名称" onChange={e => this.updateSearch('customerName', e.target.value)} />
                     </div>
                     <div className="manager-search-item">
-                        <div className="search-item__title">电话</div>
-                        <Input size="small" placeholder="请输入电话" onChange={e => this.updateSearch('phone', e.target.value)} />
+                      <div className="search-item__title">电话</div>
+                      <Input size="small" placeholder="请输入电话" onChange={e => this.updateSearch('phone', e.target.value)} />
                     </div>
-                    {/*<div className="manager-search-item">*/}
-                    {/*    <div className="search-item__title">状态</div>*/}
-                    {/*    <Select*/}
-                    {/*        style={{ width: 200 }}*/}
-                    {/*        onChange={value => this.updateSearch('status', value)}>*/}
-                    {/*        <Select.Option value="">全部</Select.Option>*/}
-                    {/*        <Select.Option value="备货中">备货中</Select.Option>*/}
-                    {/*        <Select.Option value="待发货">待发货</Select.Option>*/}
-                    {/*        <Select.Option value="已完成">已完成</Select.Option>*/}
-                    {/*    </Select>*/}
-                    {/*</div>*/}
-                    {/*<div className="manager-search-item">*/}
-                    {/*    <div className="search-item__title">时间范围</div>*/}
-                    {/*    <RangePicker onChange={(date, dateString) => {*/}
-                    {/*        this.updateSearch('startTime', dateString[0])*/}
-                    {/*        this.updateSearch('endTime', dateString[1])*/}
-                    {/*    }} />*/}
-                    {/*</div>*/}
 
                     {/*<div className="manager-search-btn"><Button onClick={pageData} type="primary" >筛选</Button></div>*/}
-                </section>
+                  </section>
+                  <section className="sear-add">
+                    <Button type="primary">新增</Button>
+                  </section>
+                </div>
+
                 <Table dataSource={dataSource} columns={columns} />
             </div>
         )
